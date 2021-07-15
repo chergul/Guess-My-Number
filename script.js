52bb5ddd85405caf5ececcc8344fe7ea502e33ef
Ceyda Hergul <ceyda_hergul@hotmail.com>
2021-07-15 16:07:16 -0400

    some code

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 'use strict';
+// -Understanding the problem
+// 1-System picks a Number
+// 2-When the user push a number it needs to Understanding3-Deal with hightscore
+// 4-Deal with again button
+
+ const secretNumber = Math.trunc(Math.random() * 20) + 1;
+ let scrore = 0;
+ let highscore = 0;
+
+
+document.querySelector('.check').addEventListener('.click', function() {
+    //Convert string to a number so we can compera with math random numbers.
+    const guessNumber = Number(document.querySelector('.guess'),value);
+    console.log(guessNumber);
+
+})
